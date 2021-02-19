d06fb0b40c34d5de77a31896167edd445ebb76da
Amir-16 <greenfitter516@gmail.com>
2021-02-19 16:12:14 +0600

    minor change

--- index.php
+++ index.php
@@ -668,11 +668,11 @@
         
 <ul class="social">
           
-<li><a href="https://phptraining.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+<li><a href="" target="_blank"><i class="fa fa-twitter "></i></a></li>
           
-<li><a href="https://phptraining.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+<li><a href="https://www.facebook.com/k.m.amirulislam.amir" target="_blank"><i class="fa fa-facebook"></i></a></li>
           
-<li><a href="https://phptraining.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+<li><a href="" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
         
 </ul>
         
